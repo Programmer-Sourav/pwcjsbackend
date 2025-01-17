8bf5c62acf726c77fbf9eabf501f030a8719a474
Sourav-nath <35804676+Programmer-Sourav@users.noreply.github.com>
2025-01-17 16:00:01 +0530

    feat: netlify functions

---
+++ netlify/functions/countries.js
@@ -0,0 +1,21 @@
+// netlify/functions/countries.js
+
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+  try {
+    const response = await axios.get('https://restfulcountries.com/api/v1/countries');
+    const countries = response.data;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ countries }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error fetching countries' }),
+    };
+  }
+};
